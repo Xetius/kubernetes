b6fbae222533467444dd5ff06be080986556d2c1
James Ravn and Yoseph Samuel <james.ravn@sky.uk>
2016-02-09 15:16:55 +0000

    Disable aws node security group ingress creation
    The aws provider creates a rule per load balancer for the node security
    group. However, this quickly runs into the AWS security group rule limit
    of 50.
    
    This change adds a feature to disable the automatic ingress creation.
    This requires that the user has setup a rule that allows all inbound
    traffic on ephemeral ports from the local VPC subnet (so load balancers
    can access it). E.g. `10.82.0.0/16 32768-65535`.
    
        [global]
        disableSecurityGroupIngress = true
    
    Limits: http://docs.aws.amazon.com/AmazonVPC/latest/UserGuide/VPC_Appendix_Limits.html#vpc-limits-security-groups

--- pkg/cloudprovider/providers/aws/aws.go
+++ pkg/cloudprovider/providers/aws/aws.go
@@ -207,6 +207,8 @@ type AWSCloudConfig struct {
 		KubernetesClusterTag string
 
 		UseKubernetesNodeTag bool
+
+		DisableSecurityGroupIngress bool
 	}
 }
 
@@ -1835,6 +1837,10 @@ func findSecurityGroupForInstance(instance *ec2.Instance) *string {
 // Open security group ingress rules on the instances so that the load balancer can talk to them
 // Will also remove any security groups ingress rules for the load balancer that are _not_ needed for allInstances
 func (s *AWSCloud) updateInstanceSecurityGroupsForLoadBalancer(lb *elb.LoadBalancerDescription, allInstances []*ec2.Instance) error {
+	if s.cfg.Global.DisableSecurityGroupIngress {
+		return nil
+	}
+
 	// Determine the load balancer security group id
 	loadBalancerSecurityGroupId := ""
 	for _, securityGroup := range lb.SecurityGroups {
